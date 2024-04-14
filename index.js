d25f2fff76f58689dd53cc6928a9f088e51226b2
MSubhan123 <167030443+MSubhan123@users.noreply.github.com>
2024-04-15 01:20:46 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,51 @@
+import inquirer from "inquirer";
+// todos arrays= done
+// function= done
+// opertion=
+let todos = ["tesla"];
+async function createTodo(todo) {
+    let ans = await inquirer.prompt({
+        type: "list",
+        message: "select an operater",
+        name: "select",
+        choices: ["add", "update", "view", "delete"]
+    });
+    if (ans.select == "add") {
+        let addTodo = await inquirer.prompt({
+            type: "input",
+            message: "add item...",
+            name: "todo",
+        });
+        todo.push(addTodo.todo);
+        console.log(todos);
+    }
+    if (ans.select == "update") {
+        let updateTodo = await inquirer.prompt({
+            type: "list",
+            message: "select item for update",
+            choices: todos.map(item => item)
+        });
+        let addTodo = await inquirer.prompt({
+            type: "list",
+            message: "add item..",
+            name: "todo",
+        });
+        let newTodos = todos.filter(val => val !== updateTodo.todo);
+        todos = [...newTodos, addTodo.todo];
+        console.log(todos);
+    }
+    if (ans.select == "view") {
+        console.log(todos);
+        if (ans.select == "delete") {
+            let deleteTodo = await inquirer.prompt({
+                type: "list",
+                message: "select item for update",
+                choices: todos.map(item => item)
+            });
+            let newTodos = todos.filter(val => val !== deleteTodo.todo);
+            todos = [...newTodos];
+            console.log(todos);
+        }
+    }
+    createTodo(todos);
+}
